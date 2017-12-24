2b57bbc44f23f509d5bf03c98ef8bdc2a466cf6c
slavals <slavals616@gmail.com>
2017-12-24 18:32:11 +0400

    конечный продукт без стилей

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-var scratchArea = document.getElementById('scareArea');
-var textRep = ['а', 'б', 'в', 'г'];
+var scratchArea = document.getElementById("scratchArea");
+var textRep = 'password123';
+var cl = console.log;
+let text = "";
+var arrText = [];
+var ids = [];
 
-function scratching() {
-    
+
+function sortTextId() {
+    for (let i = 0; i < arrText.length; i++) {
+        ids.push(arrText[i].id);
+    }
 }
+
+
+function replaceSharp() {
+    var textLength = textRep.length;
+
+    for (let i = 0; i < textLength; i++) {
+        let el = document.createElement('span');
+        let c = `scare-${i}`;
+        el.innerText = "#";
+        scratchArea.appendChild(el);
+
+        arrText.push({
+            id: i,
+            class: c,
+            text: "#",
+            original: textRep[i],
+            coord: el.getBoundingClientRect().left
+        });
+
+        el.className = `textField ${c}`;
+
+
+    }
+    sortTextId();
+}
+
+scratchArea.addEventListener("mouseover", function(event) {
+    let currentCoord = event.pageX;
+    let el = event.target;
+
+    let field = event.target;
+    let fieldClass = field.className;
+    let originalText = '';
+
+    for (let i = 0; i < arrText.length; i++) {
+        let o = arrText[i];
+        let oText = '';
+        if (`textField ${o.class}` === fieldClass) {
+            oText = o.original;
+            field.innerText = oText;
+        }
+    }
+});
+
+replaceSharp();
+
+console.table(arrText);
+cl(ids)
